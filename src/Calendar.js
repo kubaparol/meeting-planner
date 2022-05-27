72330d52e6c28194ddc473c1ba20880df6b380aa
Jakub Parol <kubciop33@gmail.com>
2022-05-27 19:46:47 +0200

    feat(Calendar): Implementation of adding new meeting and input change

--- src/Calendar.js
+++ src/Calendar.js
@@ -32,23 +32,23 @@ class Calendar extends React.Component {
     return (
       <>
         <h1>Meeting planner</h1>
-        <form>
+        <form onSubmit={this.addNewMeeting}>
           <label>First name:
-            <input type="text" value={firstName}/>
+            <input name={"firstName"} value={firstName} onChange={this.inputChange}/>
             </label>
           <label>Last name:
-            <input type="text" value={lastName}/>
+            <input name={"lastName"} value={lastName} onChange={this.inputChange}/>
             </label>
           <label>E-mail:
-            <input type="text" value={email}/>
+            <input name={"email"} value={email} onChange={this.inputChange}/>
             </label>
           <label>Date:
-            <input type="text" value={date}/>
+            <input name={"date"} value={date} onChange={this.inputChange}/>
             </label>
           <label>Time:
-            <input type="text" value={time}/>
+            <input name={"time"} value={time} onChange={this.inputChange}/>
             </label>
-            <input type="submit" value="Add" />
+            <input type="submit" value="Add"/>
         </form>
         <ul>
           {meetings.map(({firstName, lastName, email, date, time, id}) => {
@@ -66,6 +66,43 @@ class Calendar extends React.Component {
         </ul>
       </>
     )
+  }
+
+  inputChange = e => {
+    const {name, value} = e.target
+
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  addNewMeeting = e => {
+    e.preventDefault();
+
+    const {
+      firstName,
+      lastName,
+      email,
+      date,
+      time
+    } = this.state
+
+    const newMeeting = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      date: date,
+      time: time
+    }
+
+    this.setState((prevState) => ({
+      firstName: '',
+      lastName: '',
+      email: '',
+      date: '',
+      time: '',
+      meetings: prevState.meetings.concat(newMeeting),
+    }))
   }
 }
 
