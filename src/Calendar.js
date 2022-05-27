9e5a930991f2e4bdd060963bd756694cbda0de0b
Jakub Parol <kubciop33@gmail.com>
2022-05-27 19:22:36 +0200

    feat(Calendar): Add form and rendering list of meetings

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,9 +1,70 @@
 import React from 'react'
 
 class Calendar extends React.Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    date: '',
+    time: '',
+    meetings: [
+      {
+        firstName: "Jan",
+        lastName: "Kowalski",
+        email: "jan@kowalski.not",
+        date: "2022-01-01",
+        time: "10:00",
+        id: 1
+      }
+    ]
+  }
+
   render() {
+    const {
+      firstName,
+      lastName,
+      email,
+      date,
+      time,
+      meetings
+    } = this.state
+
     return (
-      <h1>Calendar</h1>
+      <>
+        <h1>Meeting planner</h1>
+        <form>
+          <label>First name:
+            <input type="text" value={firstName}/>
+            </label>
+          <label>Last name:
+            <input type="text" value={lastName}/>
+            </label>
+          <label>E-mail:
+            <input type="text" value={email}/>
+            </label>
+          <label>Date:
+            <input type="text" value={date}/>
+            </label>
+          <label>Time:
+            <input type="text" value={time}/>
+            </label>
+            <input type="submit" value="Add" />
+        </form>
+        <ul>
+          {meetings.map(({firstName, lastName, email, date, time, id}) => {
+            return (
+              <li key={id}>
+                <header>
+                  <h2>{firstName} {lastName}</h2>
+                  <p>{email}</p>
+                </header>
+                <p>{date} {time}</p>
+                <button>Delete</button>
+              </li>
+            )
+          })}
+        </ul>
+      </>
     )
   }
 }
